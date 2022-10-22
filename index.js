f490f86f5348d182fd9b0803129bb97bb42dbf8c
enriqueteru <info@enriqueteruel.com>
2022-10-22 16:54:54 +0200

    init(generator-nft)

---
+++ index.js
@@ -0,0 +1,15 @@
+const initial = async () => {
+    var _thisIndex = 1;
+    const _maxSupply = 50;
+    while(_thisIndex <= _maxSupply) {
+        try{
+            console.log('Generating NFT '+_thisIndex);
+            _thisIndex++;
+        }catch(e){
+            console.error('Error while generating NFT '+_thisIndex)
+            console.log(e);
+            _thisIndex = _maxSupply + 1;
+        }
+    }
+}
+initial();
